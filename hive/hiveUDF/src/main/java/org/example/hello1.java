9927643ba3584ea932590b75b9e092e4605e3654
cj <cj@example.com>
2023-05-02 10:52:52 +0800

    修改一个hello1类 版本：ver4

--- hive/hiveUDF/src/main/java/org/example/hello1.java
+++ hive/hiveUDF/src/main/java/org/example/hello1.java
@@ -3,5 +3,8 @@ package org.example;
 public class hello1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
